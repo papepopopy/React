eabc62cf8ee5f68fd2226d0d7608b4a37bfc40be
hapepopo <167487410+hapepopo@users.noreply.github.com>
2024-05-29 18:11:58 +0900

    style

--- src/component/Controller.js
+++ src/component/Controller.js
@@ -1,13 +1,13 @@
 
 const Controller = ({handleSetCount1}) => {
     return(
-        <div className="controller">
-            <button onClick={() => handleSetCount1(-1)}>-1</button>
-            <button onClick={() => handleSetCount1(-10)}>-10</button>
-            <button onClick={() => handleSetCount1(-100)}>-100</button>
-            <button onClick={() => handleSetCount1(100)}>+100</button>
-            <button onClick={() => handleSetCount1(10)}>+10</button>
-            <button onClick={() => handleSetCount1(1)}>+1</button>
+        <div className="controller btn-group">
+            <button className="btn btn-danger" onClick={() => handleSetCount1(-1)}>-1</button>
+            <button className="btn btn-danger" onClick={() => handleSetCount1(-10)}>-10</button>
+            <button className="btn btn-danger" onClick={() => handleSetCount1(-100)}>-100</button>
+            <button className="btn btn-danger" onClick={() => handleSetCount1(100)}>+100</button>
+            <button className="btn btn-primary" onClick={() => handleSetCount1(10)}>+10</button>
+            <button className="btn btn-danger" onClick={() => handleSetCount1(1)}>+1</button>
         </div>
     )
 }
